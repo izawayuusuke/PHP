4eb4053d91aacc45311b670c82781455d6e5c723
izawa yuusuke <yuu05020@gmail.com>
2020-08-14 19:48:56 +0900

    連想配列を定義する

---
+++ object.php
@@ -0,0 +1,17 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP入門</title>
+</head>
+<body>
+  <?php
+  $fruit = array("Apple" => "りんご", "Orange" => "みかん",
+      "Grape" => "ぶどう");
+    while(list($key, $val) = each($fruit)) {
+      print("インデックスの $key は、$val です。<br>\n");
+    }
+  ?>
+</body>
+</html>
