2091a57047f850b94e9a799022f8bf9b882ebac2
izawa yuusuke <yuu05020@gmail.com>
2020-08-14 19:11:08 +0900

    定数を定義する

---
+++ const.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP入門</title>
+</head>
+<body>
+  <?php
+  define("banban", "ばんばん");
+  print(banban);
+  ?>
+</body>
+</html>
