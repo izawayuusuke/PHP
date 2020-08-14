fddc40d02042c18059b8f6e5a5bbb195e99bd8ac
izawa yuusuke <yuu05020@gmail.com>
2020-08-14 18:53:25 +0900

    PHPを埋め込む

---
+++ date.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP入門</title>
+</head>
+<body>
+  <p>今日は、<?php echo date("Y/m/d"); ?>です。</p>
+</body>
+</html>
