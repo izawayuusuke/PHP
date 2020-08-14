1fd6f6778f9122b420061031ed7c120eba2975a3
izawa yuusuke <yuu05020@gmail.com>
2020-08-14 19:26:47 +0900

    スカラー型変数を定義する

---
+++ variable.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP入門</title>
+</head>
+<body>
+  <?php
+  $boolean = true;
+  $integer = 2000;
+  $float = 3.14151963;
+  $string = "ばんばんは当サイトのハンドルネームです。";
+  print($boolean);
+  print($integer);
+  print($float);
+  print($string);
+  ?>
+</body>
+</html>
