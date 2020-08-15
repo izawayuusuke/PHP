db63f25b368772ddf7e448a1ad0ee3643fe39204
izawa yuusuke <yuu05020@gmail.com>
2020-08-15 19:52:48 +0900

    文字列データの生成

---
+++ string.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html lang="ja">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>PHP入門</title>
+</head>
+<body>
+  <?php
+  $str = "初心者のためのPHP入門<br>\n文字列の項目";
+  print($str);
+  ?>
+</body>
+</html>
