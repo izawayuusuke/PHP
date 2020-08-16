3c2444321446467de3a1423faba7722119d5c10f
izawa yuusuke <yuu05020@gmail.com>
2020-08-16 18:07:07 +0900

    switch構文

--- branch.php
+++ branch.php
@@ -20,6 +20,21 @@
   } else {
     print("変数\$bは、5よりも小さい<br>\n");
   }
+
+  $age = 20;
+  switch($age) {
+    case "15":
+      print("勉強に励んでください");
+      break;
+    case "20":
+      print("若くて羨ましいです");
+      break;
+    case 30:
+      print("これから人生の本番です");
+      break;
+    default:
+      print("人生は悩み多きものです");
+  }
   ?>
 </body>
 </html>
