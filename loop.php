d2af9f5479a46d70b14daa670b8e852ed2e02930
izawa yuusuke <yuu05020@gmail.com>
2020-08-16 17:10:57 +0900

    break文でループを抜ける

--- loop.php
+++ loop.php
@@ -15,17 +15,27 @@
     $a++;
   }
 
-  // for構文
+  // for構文 + break
   for($a = 0; $a < 4; $a++) {
     print($lectur[$a]. "&#160;\n");
+    if($a == 3) {
+      print("ループを抜けます<br>\n");
+      break;
+    }
   }
 
-  //do...while構文
+  // do...while構文
   $a = 0;
   do {
-    print($lectur[$a]. "&#160;\n");
+    print($lectur[$a]. "&#160;<br>\n");
     $a++;
   } while($a < 4);
+
+  // foreach構文
+  $a = array("Apple" => "りんご", "Orange" => "みかん", "Grape" => "ぶどう");
+  foreach($a as $key => $value) {
+    print("${key}は、${value}<br>\n");
+  }
   ?>
 </body>
 </html>
