56ad7db8708d9388bee5f41185129ae5c51365c3
izawa yuusuke <yuu05020@gmail.com>
2020-08-17 14:49:59 +0900

    クラスの定義

--- object.php
+++ object.php
@@ -12,6 +12,22 @@
     while(list($key, $val) = each($fruit)) {
       print("インデックスの${key}は、${val}です。<br>\n");
     }
+
+    class Shopping {
+      // メンバー変数の設定
+      var $item = "コンピュータ";
+      var $price = 99800;
+
+      // メソッドの定義
+      function getTotal() {
+        $tax = $this -> price * 0.05;
+        return $this -> price + $tax;
+      }
+    }
+
+    $newShopping = new Shopping();
+    $item = $newShopping -> item;
+    print("${item}の会計は".$newShopping -> getTotal()."円になります<br>\n");
   ?>
 </body>
 </html>
